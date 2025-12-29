7f1548cbd2410ed4b0fca77043bdcfb6e601b1ad
Awwiiiii <awnishverma07@gmail.com>
2025-12-30 02:05:40 +0530

    First commit: Add Terraform and Infracost workflow

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# This is a 'pricey' instance to test our FinOps pipeline
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "m5.4xlarge" # This costs roughly $500+/month
+
+  root_block_device {
+    volume_size = 100 
+  }
+}
